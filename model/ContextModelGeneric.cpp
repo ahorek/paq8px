3ecf2f4f101750e08fec3461fcb78cf7235bff03
Pavel Rosický <pavel.rosicky@easy.cz>
2023-03-14 19:56:21 +0100

    mini

--- model/ContextModelGeneric.cpp
+++ model/ContextModelGeneric.cpp
@@ -13,28 +13,13 @@ public:
     const bool useLSTM = shared->GetOptionUseLSTM();
     m = mf->createMixer(
       1 +  //bias
-      MatchModel::MIXERINPUTS + NormalModel::MIXERINPUTS + SparseMatchModel::MIXERINPUTS +
-      SparseModel::MIXERINPUTS + SparseBitModel::MIXERINPUTS + ChartModel::MIXERINPUTS +
-      RecordModel::MIXERINPUTS + CharGroupModel::MIXERINPUTS +
-      TextModel::MIXERINPUTS + WordModel::MIXERINPUTS_BIN + IndirectModel::MIXERINPUTS +
-      DmcForest::MIXERINPUTS + NestModel::MIXERINPUTS + XMLModel::MIXERINPUTS +
-      LinearPredictionModel::MIXERINPUTS + ExeModel::MIXERINPUTS +
+      NormalModel::MIXERINPUTS +
       (useLSTM ? LstmModel<>::MIXERINPUTS : 0)
       ,
-      MatchModel::MIXERCONTEXTS + NormalModel::MIXERCONTEXTS_PRE + NormalModel::MIXERCONTEXTS_POST + SparseMatchModel::MIXERCONTEXTS +
-      SparseModel::MIXERCONTEXTS + SparseBitModel::MIXERCONTEXTS + ChartModel::MIXERCONTEXTS +
-      RecordModel::MIXERCONTEXTS + CharGroupModel::MIXERCONTEXTS +
-      TextModel::MIXERCONTEXTS + WordModel::MIXERCONTEXTS + IndirectModel::MIXERCONTEXTS +
-      DmcForest::MIXERCONTEXTS + NestModel::MIXERCONTEXTS + XMLModel::MIXERCONTEXTS +
-      LinearPredictionModel::MIXERCONTEXTS + ExeModel::MIXERCONTEXTS +
+      NormalModel::MIXERCONTEXTS_PRE + NormalModel::MIXERCONTEXTS_POST +
       (useLSTM ? LstmModel<>::MIXERCONTEXTS : 0)
       ,
-      MatchModel::MIXERCONTEXTSETS + NormalModel::MIXERCONTEXTSETS_PRE + NormalModel::MIXERCONTEXTSETS_POST + SparseMatchModel::MIXERCONTEXTSETS +
-      SparseModel::MIXERCONTEXTSETS + SparseBitModel::MIXERCONTEXTSETS + ChartModel::MIXERCONTEXTSETS +
-      RecordModel::MIXERCONTEXTSETS + CharGroupModel::MIXERCONTEXTSETS +
-      TextModel::MIXERCONTEXTSETS + WordModel::MIXERCONTEXTSETS + IndirectModel::MIXERCONTEXTSETS +
-      DmcForest::MIXERCONTEXTSETS + NestModel::MIXERCONTEXTSETS + XMLModel::MIXERCONTEXTSETS +
-      LinearPredictionModel::MIXERCONTEXTSETS + ExeModel::MIXERCONTEXTSETS + 
+      NormalModel::MIXERCONTEXTSETS_PRE + NormalModel::MIXERCONTEXTSETS_POST +
       (useLSTM ? LstmModel<>::MIXERCONTEXTSETS : 0)
       ,
       (useLSTM ? 1 : 0)
@@ -50,46 +35,11 @@ public:
     normalModel.mix(*m);
     normalModel.mixPost(*m);
 
-    MatchModel& matchModel = models->matchModel();
-    matchModel.mix(*m);
-
     const bool useLSTM = shared->GetOptionUseLSTM();
     if (useLSTM) {
       LstmModel<>& lstmModel = models->lstmModelGeneric();
       lstmModel.mix(*m);
     }
-
-    SparseMatchModel& sparseMatchModel = models->sparseMatchModel();
-    sparseMatchModel.mix(*m);
-    SparseBitModel& sparseBitModel = models->sparseBitModel();
-    sparseBitModel.mix(*m);
-    SparseModel& sparseModel = models->sparseModel();
-    sparseModel.mix(*m);
-    ChartModel& chartModel = models->chartModel();
-    chartModel.mix(*m);
-    RecordModel& recordModel = models->recordModel();
-    recordModel.mix(*m);
-    CharGroupModel& charGroupModel = models->charGroupModel();
-    charGroupModel.mix(*m);
-    TextModel& textModel = models->textModel();
-    textModel.mix(*m);
-    WordModel& wordModel = models->wordModel();
-    wordModel.mix(*m);
-    IndirectModel& indirectModel = models->indirectModel();
-    indirectModel.mix(*m);
-    DmcForest& dmcForest = models->dmcForest();
-    dmcForest.mix(*m);
-    NestModel& nestModel = models->nestModel();
-    nestModel.mix(*m);
-    XMLModel& xmlModel = models->xmlModel();
-    xmlModel.mix(*m);
-
-    LinearPredictionModel& linearPredictionModel = models->linearPredictionModel();
-    linearPredictionModel.mix(*m);
-
-    //exemodel must be the last
-    ExeModel& exeModel = models->exeModel();
-    exeModel.mix(*m);
 
     return m->p();
   }
--- model/ContextModelText.cpp
+++ model/ContextModelText.cpp
@@ -13,25 +13,13 @@ public:
     const bool useLSTM = shared->GetOptionUseLSTM();
     m = mf->createMixer (
       1 +  //bias
-      MatchModel::MIXERINPUTS + NormalModel::MIXERINPUTS + SparseMatchModel::MIXERINPUTS +
-      SparseModel::MIXERINPUTS_TEXT + SparseBitModel::MIXERINPUTS_TEXT + ChartModel::MIXERINPUTS_TEXT +
-      RecordModel::MIXERINPUTS + CharGroupModel::MIXERINPUTS +
-      TextModel::MIXERINPUTS + WordModel::MIXERINPUTS_TEXT + IndirectModel::MIXERINPUTS_TEXT +
-      DmcForest::MIXERINPUTS + NestModel::MIXERINPUTS + XMLModel::MIXERINPUTS +
+      NormalModel::MIXERINPUTS +
       (useLSTM ? LstmModel<>::MIXERINPUTS : 0)
       ,
-      MatchModel::MIXERCONTEXTS + NormalModel::MIXERCONTEXTS_PRE + NormalModel::MIXERCONTEXTS_POST + SparseMatchModel::MIXERCONTEXTS +
-      SparseModel::MIXERCONTEXTS_TEXT + SparseBitModel::MIXERCONTEXTS + ChartModel::MIXERCONTEXTS +
-      RecordModel::MIXERCONTEXTS + CharGroupModel::MIXERCONTEXTS +
-      TextModel::MIXERCONTEXTS + WordModel::MIXERCONTEXTS + IndirectModel::MIXERCONTEXTS +
-      DmcForest::MIXERCONTEXTS + NestModel::MIXERCONTEXTS + XMLModel::MIXERCONTEXTS +
+      NormalModel::MIXERCONTEXTS_PRE + NormalModel::MIXERCONTEXTS_POST +
       (useLSTM ? LstmModel<>::MIXERCONTEXTS : 0)
       ,
-      MatchModel::MIXERCONTEXTSETS + NormalModel::MIXERCONTEXTSETS_PRE + NormalModel::MIXERCONTEXTSETS_POST + SparseMatchModel::MIXERCONTEXTSETS +
-      SparseModel::MIXERCONTEXTSETS_TEXT + SparseBitModel::MIXERCONTEXTSETS + ChartModel::MIXERCONTEXTSETS +
-      RecordModel::MIXERCONTEXTSETS + CharGroupModel::MIXERCONTEXTSETS +
-      TextModel::MIXERCONTEXTSETS + WordModel::MIXERCONTEXTSETS + IndirectModel::MIXERCONTEXTSETS +
-      DmcForest::MIXERCONTEXTSETS + NestModel::MIXERCONTEXTSETS + XMLModel::MIXERCONTEXTSETS +
+      NormalModel::MIXERCONTEXTSETS_PRE + NormalModel::MIXERCONTEXTSETS_POST +
       (useLSTM ? LstmModel<>::MIXERCONTEXTSETS : 0)
       ,
       (useLSTM ? 1 : 0)
@@ -48,9 +36,6 @@ public:
     normalModel.mix(*m);
     normalModel.mixPost(*m);
     
-    MatchModel& matchModel = models->matchModel();
-    matchModel.mix(*m);
-
     const bool useLSTM = shared->GetOptionUseLSTM();
     if (useLSTM) {
       LstmModel<>& lstmModel = models->lstmModelText();
@@ -58,33 +43,6 @@ public:
     }
 
     INJECT_SHARED_blockType
-
-    SparseMatchModel& sparseMatchModel = models->sparseMatchModel();
-    sparseMatchModel.mix(*m);
-    SparseBitModel& sparseBitModel = models->sparseBitModel();
-    sparseBitModel.mix(*m);
-    if (blockType != BlockType::DBF) {
-      SparseModel& sparseModel = models->sparseModel();
-      sparseModel.mix(*m);
-      XMLModel& xmlModel = models->xmlModel();
-      xmlModel.mix(*m);
-      NestModel& nestModel = models->nestModel();
-      nestModel.mix(*m);
-    }
-    ChartModel& chartModel = models->chartModel();
-    chartModel.mix(*m);
-    RecordModel& recordModel = models->recordModel();
-    recordModel.mix(*m);
-    CharGroupModel& charGroupModel = models->charGroupModel();
-    charGroupModel.mix(*m);
-    TextModel& textModel = models->textModel();
-    textModel.mix(*m);
-    WordModel& wordModel = models->wordModel();
-    wordModel.mix(*m);
-    IndirectModel& indirectModel = models->indirectModel();
-    indirectModel.mix(*m);
-    DmcForest& dmcForest = models->dmcForest();
-    dmcForest.mix(*m);
 
     return m->p();
   }
